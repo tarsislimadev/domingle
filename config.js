48a7d386aacd179f646af74fc81d6fa4a8ae1ae7
Tarsis Lima <br.tmvdl@gmail.com>
2025-10-18 21:06:03 -0300

    Update configuration file

--- config.js
+++ config.js
@@ -1,16 +1,10 @@
-const getAppName = () => {
-    const { hostname } = new URL(window.location.href)
-    const isIP = hostname.split('.').every((n) => !isNaN(parseInt(n)))
-    return (isIP ? hostname : hostname.split('.').at(0))
-}
+const getHostname = () => new URL(window.location.href).hostname
+
+const getAppName = () => ['d', getHostname().split('.').join('')].join('-')
 
 export const getName = (suffix) => [getAppName(), suffix].join('-')
 
 export const getAdminID = () => getName('admin')
-
-console.log('ADMIN', getAdminID())
-
-console.log('NAME', getName('name'))
 
 export const TIMEOUT = 2000
 
@@ -18,10 +12,12 @@ export const peer_config = {
   config: {
     'iceServers': [
       {
-        'urls': 'stun:0.0.0.0:4224'
+        'urls': `stun:${getHostname()}:4224`
       }
     ],
     'sdpSemantics': 'unified-plan'
   }
 }
 
+console.log('ADMIN', getAdminID())
+console.log('NAME', getName('name'))
